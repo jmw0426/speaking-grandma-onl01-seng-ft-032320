d6f04673aab67883f7aa632a92f30aa7632a6837
Joshua Williams <jmw0426@hotmail.com>
2020-03-24 16:22:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     
   elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1983!"
+    return "NO, NOT SINCE 1938!"
     
   else 
     return "HUH?! SPEAK UP, SONNY!"
